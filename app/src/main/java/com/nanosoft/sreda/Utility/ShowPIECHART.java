5f7f87f9301f22f6da8f95bbad0882e21ca8bef3
Azharul Islam <azadbasis@gmail.com>
2017-12-05 18:31:08 +0600

    capacity,fuel pie data set

--- app/src/main/java/com/nanosoft/sreda/Utility/ShowPIECHART.java
+++ app/src/main/java/com/nanosoft/sreda/Utility/ShowPIECHART.java
@@ -57,7 +57,9 @@ public class ShowPIECHART {
 
         PieData data;
         final ArrayList<String> xVals;
+        final ArrayList<Integer> colorcode = null;
         final ArrayList<Entry> yvalues;
+
         pieChart.setUsePercentValues(true);
 
         xVals = new ArrayList<String>();
@@ -68,6 +70,8 @@ public class ShowPIECHART {
             float f = (float) d;
             yvalues.add(new Entry(f, i));
             xVals.add(listCapacityInfo.get(i).getTechnology_name());
+            colorcode.add(Integer.valueOf(listCapacityInfo.get(i).getColor()));
+
         }
 
         PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
@@ -82,7 +86,7 @@ public class ShowPIECHART {
         pieChart.setHoleRadius(25f);
 
 
-        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        dataSet.setColors(colorcode);
         data.setValueTextSize(8f);
         data.setValueTextColor(Color.DKGRAY);
 
